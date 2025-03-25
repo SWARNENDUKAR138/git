3c7ae8e6527cf0e8136bfb96051adf047d0298e9
SWARNENDUKAR138 <swarnendukar99@gmail.com>
2025-03-25 10:58:37 +0530

    added test files

--- testing.py
+++ testing.py
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
 print("hello friends") 
 i am bokachodda
+=======
+def hello():
+    """
+    this is demo
+    """
+    return "hello dosto"
+
+
+hello()
+>>>>>>> 47cdc56 (added test files)
